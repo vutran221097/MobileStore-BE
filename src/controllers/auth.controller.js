233ceb8079faeb5d14b7d42ac7cc43a9cf115f2f
tranvu221097@gmail.com <tranvu221097@gmail.com>
2021-08-09 13:25:20 +0700

    a

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -127,6 +127,7 @@ exports.signin = (req, res) => {
       for (let i = 0; i < user.roles.length; i++) {
         authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
       }
+      
       res.status(200).send({
         id: user._id,
         username: user.username,
